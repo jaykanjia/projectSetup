d6a8b3179dd99de0d08acdaafc05c8430e8fa970
JayKanjia <jaykanjia11@gmail.com>
2023-07-05 18:03:35 +0530

    setup for html and css done

--- src/index.js
+++ src/index.js
@@ -1,2 +1,4 @@
+import './style.css';
+
 const val = 'hello world';
 console.log(val);
--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,8 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
+  mode: 'production',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
@@ -14,6 +15,16 @@ module.exports = {
         exclude: /node_modules/,
         use: 'babel-loader',
       },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+  ],
 };
